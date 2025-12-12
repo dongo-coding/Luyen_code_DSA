789b78ad301630bb187a0e53e5954ce58ffc5395
Do Ngo <bahuynh619@gmail.com>
2025-12-12 21:04:36 +0700

    Update Compressed_String.cpp

--- Compressed_String.cpp
+++ Compressed_String.cpp
@@ -1,10 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-string to_string(long long n){
+string to_string (long long n){
     string s = "";
     while(n>0){
-        s=char(n%10 + '0') + s;
+        s+=char(n%10 + '0');
         n/=10;
     }
     return s;
@@ -13,25 +13,25 @@ string to_string(long long n){
 int main ()
 {
     string s;
-    getline (cin,s);
-    s = s +'@';
-    stack <char>st;
-    string str = "";
+    getline(cin,s);
+    s+='@';
+    stack<char>st;
+    string ss="";
     for(int i=0;i<s.length();i++){
-        if (st.empty () || st.top() == s[i]){
+        if(st.empty() || st.top() == s[i]){
             st.push(s[i]);
         }
         else {
             int cnt=0;
-            str += st.top();
-            while (!st.empty()){
+            ss+= st.top(); 
+            while(!st.empty()){
                 cnt++;
                 st.pop();
             }
-            str+=to_string(cnt);
+            ss+=to_string(cnt);
             st.push(s[i]);
+
         }
     }
-    cout<<str;
-
+    cout<<ss;
 }
